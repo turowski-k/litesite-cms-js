6a0626380fbf6ab77dd806f1912c5713f5485ebe
Kacper Turowski <turowski.kacper@gmail.com>
2022-09-16 17:35:47 +0200

    add comments to route solver

--- src/litesitejs/modules/router.js
+++ src/litesitejs/modules/router.js
@@ -19,19 +19,24 @@ function onChangeRoute(event) {
 
 function resolveRoute(requestedRoute, queryParams) {
     for (const route of routes) {
+        // TODO: This might call for rework, possibly order of if-s is wrong, maybe should check *, :, === instead?
         let match = true;
+        // check against routes one by one, return first match
+        // (registering route order is important, start from most specific)
         for (let i = 0; i < route.path.length; i++) {
-            // check for exact
+            // check if route node calls for exact match and is matching
             if (route.path[i] === requestedRoute[i]) {
                 continue;
             }
-            // check for param
+            // check if route node calls for parameter and assign it
             else if (route.path[i][0] === ':') {
                 const nodeKey = route.path[i].slice(1);
                 queryParams[nodeKey] = requestedRoute[i];
                 continue;
             }
-            // check for star (wildcard, ignore this and all beyond)
+            // check if route node is a wildcard - if so,
+            // then it's a match and everything beyond is ignored
+            // useful for creating semantic urls like #/blog/1/my-awesome-post-name
             else if (route.path[i] === '*') {
                 break;
             }
